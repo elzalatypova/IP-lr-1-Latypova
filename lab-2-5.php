9e795f00140cfe725859ac8e25fa5e04f170823e
elzalatypova <79845168+elzalatypova@users.noreply.github.com>
2021-11-22 05:47:49 +0500

    Update lab-2-5.php

--- lab-2-5.php
+++ lab-2-5.php
@@ -17,4 +17,22 @@
     echo "b = $b" . '<br>';
     echo "z = $z" . '<br>';
 
+echo  "Вариант №14" . '<br>';
+ function f6($u, $t)
+    {
+        if ($u < (-2))
+            return $u * 2;
+        elseif ($u <= 2 && $u >= (-2))
+            return (sin(pow($u, 3)) + log(abs($t), 10));
+        elseif ($u > 2)
+            return sqrt(cos(pow($u, 2)) + (pow(sin(pow($t, 4)),4);}
+    }
+    $a = rand(1, 20);
+    $b = rand(1, 20);
+    $z = f6($a / $b, (pow($b, 8) - pow($a, 7)) / ($a * $b)) + f6((pow($a, 10) + pow($b,12)) / ($a * pow($b, 2) - $a), $b);
+    echo "a = $a" . '<br>';
+    echo "b = $b" . '<br>';
+    echo "z = $z" . '<br>';
+?>
+
 ?>
