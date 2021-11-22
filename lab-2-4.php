ac040e62a0fa0410fe01d0b1f0b5f3e719d4a865
elzalatypova <79845168+elzalatypova@users.noreply.github.com>
2021-11-22 05:47:01 +0500

    Add files via upload

--- lab-2-4.php
+++ lab-2-4.php
@@ -1,14 +1,47 @@
 <h3>Латыпова Эльза</h3>
-<?php
-$dn=rand(1,7); 
-print ($dn ." - номер дня недели <br>");
-switch ($dn) {
-case 1: print ("это понедельник"); break;
-case 2: print ("это вторник"); break;
-case 3: print ("это среда"); break;
-case 4: print ("это четверг"); break;
-case 5: print ("это пятница"); break;
-case 6: print ("это суббота"); break;
-case 7: print ("это воскресенье");
-}
+ <p>Вариант №14</p>
+    <?php
+    $A = array();
+    $N = rand(1, 20);
+    $K = 20;
+    $min = 20;
+for ($i = 0; $i < $N; $i++) {
+        $A[] = rand(1,20);}
+
+    echo "</br> Исходный массив:  </br>";
+    foreach($A as $value) {
+    echo "$value ";}
+    for ($i = 0; $i < $N; $i++) {
+        if ($A[$i] < $min) {
+	    $min =  $A[$i];
+        }
+    }
+    for ($j = 0; $j < $N; $j++) {
+        if ($A[$j] > $min) {
+            $A[$j] = $min;
+        }
+    }
+ echo "</br> Отсортированный массив: </br>";
+    foreach($A as $value) {  
+    echo "$value ";}
 ?>
+
+<p>Вариант №4</p>
+    <?php
+    $A = array();
+    $N = rand(1, 10);
+    $K = 0;
+for ($i = 0; $i < $N; $i++) {
+        $A[] = rand(1,20);}
+
+    echo "</br> Исходный массив:  </br>";
+    foreach($A as $value) {
+    echo "$value ";}
+    for ($i = 0; $i < $N; $i++) {
+        if ($A[$i] > 0) {
+	    $K =  $K+1;
+        }
+    }
+ echo "</br> Макс-ое число подряд идущих полож-ых эл-ов: $K";
+?>
+    
--- lab-2-5.php
+++ lab-2-5.php
@@ -1,10 +1,20 @@
 <h3>Латыпова Эльза</h3>
+<p>Вариант №6</p>
 <?php
-$var = 5; $i = 0;
-while (++$i <= $var)
- { echo $i . ' ' ;}
-echo '<br>';
-$var = 0;
-while (--$i > $var)
- { echo $i . ' '; };
+    function f14($u, $t) {
+        if ($u >= 0 && $t >= 0)
+            return $u + $t;
+        elseif ($u < 0 && $t >= 0)
+            return $u * $u + $t;
+        elseif ($u >= 0 && $t < 0)
+            return ($u - 2 * $t);
+        elseif ($u < 0 && $t < 0)
+            return ($t + 3 * $u) / ($u * $t);}
+    $a = rand(1, 20);
+    $b = rand(1, 20);
+    $z = f14($a, pow($b, 8) - pow($a, 7)) + f14(pow($a, 10) - pow($b, 11), $b);
+    echo "a = $a" . '<br>';
+    echo "b = $b" . '<br>';
+    echo "z = $z" . '<br>';
+
 ?>
